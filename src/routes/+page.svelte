e16efd7634c1590435804746e2ae42759f37806d
Thomasevano <thomas-evano@hotmail.fr>
2024-02-05 17:54:14 +0100

    feat: add HeroSection to landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Navigation from "$lib/components/landing/Navigation.svelte";
-
+  import HeroSection from "$lib/components/landing/HeroSection.svelte";
 </script>
 
 <Navigation/>
+<HeroSection/>
