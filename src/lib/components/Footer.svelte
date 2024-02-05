a18f13972be735b3efe079e820990d88ea01147b
Thomasevano <thomas-evano@hotmail.fr>
2024-02-05 19:55:24 +0100

    feat: add Footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,8 @@
+<footer>
+	<span class="block text-sm text-muted-foreground sm:text-center">
+		© {new Date().getFullYear()}{' '}
+    With ❤️ by
+		<a target="_blank" href="https://thomasevano.fr/" class="hover:underline"> Evano Thomas </a>
+		. All Rights Reserved.
+	</span>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
 	import '../app.pcss';
 	import { ModeWatcher } from 'mode-watcher';
+	import Footer from '$lib/components/Footer.svelte';
 </script>
 
 <slot />
 <ModeWatcher />
+<Footer />
