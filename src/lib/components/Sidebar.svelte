9eb71da4355bdf6e3d3e008c209757349b18db69
Thomasevano <thomas-evano@hotmail.fr>
2024-02-07 23:20:27 +0100

    feat: add avatar to sidebar

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -1,14 +1,29 @@
 <script lang="ts">
-	import { cn } from "$lib/utils";
-	import { Button } from '$lib/components/ui/button'
+	import { cn } from '$lib/utils';
+	import { Button } from '$lib/components/ui/button';
+	import * as Avatar from '$lib/components/ui/avatar';
 
 	let className: string | null | undefined = undefined;
 	export { className as class };
+
+	export let user: SpotifyApi.CurrentUsersProfileResponse;
 </script>
 
-<div class={cn("pb-12", className)}>
+<div class={cn('pb-12', className)}>
 	<div class="space-y-4 py-4">
 		<div class="px-3 py-2">
+			<div class="mb-4 flex items-center space-x-4">
+				<Avatar.Root>
+					{#if user.images}
+						<Avatar.Image src={user.images[0].url} alt={user.display_name} />
+					{/if}
+					<Avatar.Fallback>{`${user.display_name.charAt(0)+user.display_name.charAt(1)}`}</Avatar.Fallback>
+				</Avatar.Root>
+				<div>
+					<p class="text-sm font-medium leading-none">{user.display_name}</p>
+					<p class="text-sm text-muted-foreground">{user.email}</p>
+				</div>
+			</div>
 			<h2 class="mb-2 px-4 text-lg font-semibold tracking-tight">Library</h2>
 			<div class="space-y-1">
 				<Button variant="ghost" href="#" class="w-full justify-start">
