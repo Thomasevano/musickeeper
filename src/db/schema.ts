1ae307bfe92a3ebebaf50ebea69a565cd7751a1a
Thomasevano <thomas-evano@hotmail.fr>
2024-02-16 23:04:43 +0100

    chore(auth/database): add drizzle orm and connection to turso db

---
+++ src/db/schema.ts
@@ -0,0 +1,26 @@
+import { text, integer, sqliteTable } from 'drizzle-orm/sqlite-core';
+
+export const tableNames = {
+	user: 'user',
+	session: 'session',
+	OAuthAccount: 'oauth_account'
+};
+
+export const userTable = sqliteTable(tableNames.user, {
+	id: text('id').notNull().primaryKey(),
+	username: text('username')
+});
+
+export const sessionTable = sqliteTable(tableNames.session, {
+	id: text('id').notNull().primaryKey(),
+	userId: text('user_id')
+		.notNull()
+		.references(() => userTable.id),
+	expiresAt: integer('expires_at').notNull()
+});
+
+export const OAuthAccountTable = sqliteTable(tableNames.OAuthAccount, {
+	providerId: text('provider_id').notNull().primaryKey(),
+	providerUserId: text('provider_user_id').notNull(),
+	userId: text('user_id').references(() => userTable.id)
+});
