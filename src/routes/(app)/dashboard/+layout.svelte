20ab92cccad90a0ab68ee11943b448c8348f7258
Thomasevano <thomas-evano@hotmail.fr>
2024-02-25 11:38:08 +0100

    feat(app): Move connected menu to navbar

--- src/routes/(app)/dashboard/+layout.svelte
+++ src/routes/(app)/dashboard/+layout.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
 	import type { LayoutData } from './$types';
 	import Sidebar from '$lib/components/Sidebar.svelte';
-
+	import Navigation from '$lib/components/Navigation.svelte';
 	export let data: LayoutData;
-	console.log(data);
 </script>
 
+<Navigation user={data} />
 {#if data}
 	<div class="hidden md:block">
 		<!-- <Menu /> -->
 		<div class="border-t">
 			<div class="bg-background">
 				<div class="grid lg:grid-cols-5">
-					<Sidebar class="hidden md:block" user={data} />
+					<Sidebar class="hidden md:block" />
 					<slot />
 				</div>
 			</div>
