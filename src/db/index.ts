01a3a88d851d2c6841bd3dcf4102f6fc18c8f8a5
Thomasevano <thomas-evano@hotmail.fr>
2024-03-15 15:43:10 +0100

    chore(build): specify node version and handle some env variables

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,11 +1,11 @@
-import { TURSO_DATABASE_URL, TURSO_AUTH_TOKEN } from '$env/static/private'
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
 import * as schema from './schema';
+import 'dotenv/config'
 
 export const tursoConnection = createClient({
-	url: TURSO_DATABASE_URL!,
-	authToken: TURSO_AUTH_TOKEN
+	url: process.env.TURSO_DATABASE_URL!,
+	authToken: process.env.TURSO_AUTH_TOKEN
 });
 
 export const db = drizzle(tursoConnection, { schema });
