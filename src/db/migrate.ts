cb1345d0216adfa234ba389d165b88ed24cd3658
Thomasevano <thomas-evano@hotmail.fr>
2024-03-15 15:43:10 +0100

    chore(build): specify node version and handle some env variables

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -1,6 +1,19 @@
 import { migrate } from 'drizzle-orm/libsql/migrator';
 import { db, tursoConnection } from './index';
 
-await migrate(db, { migrationsFolder: './migrations' });
+async function main() {
+  console.log('Running migrations')
+  await migrate(db, { migrationsFolder: 'src/db/migrations' });
+  console.log('Migrated successfully')
 
-tursoConnection.close();
+  tursoConnection.close();
+  process.exit(0)
+}
+
+main().catch((e) => {
+  console.error('Migration failed')
+  console.error(e)
+  process.exit(1)
+});
+
+
