e519368cb0c74662bf0c285812fe15ed766a9d35
Thomasevano <thomas-evano@hotmail.fr>
2024-06-23 14:48:34 +0200

    fix: error 500 while connecting for the first time with spotify

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,9 +4,11 @@ import 'dotenv/config'
 export default {
 	schema: './src/db/schema.ts',
 	out: './src/db/migrations',
+	dialect: 'sqlite',
 	driver: 'turso',
 	dbCredentials: {
 		url: process.env.TURSO_DATABASE_URL!,
 		authToken: process.env.TURSO_AUTH_TOKEN
-	}
+	},
+	verbose: true
 } satisfies Config;
