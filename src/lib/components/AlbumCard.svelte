7ac1bd1eeed2bbad0baee1355ed04e08d824b277
Thomasevano <thomas-evano@hotmail.fr>
2024-06-30 00:39:23 +0200

    refacto/style: playlists page with albumCard

--- src/lib/components/AlbumCard.svelte
+++ src/lib/components/AlbumCard.svelte
@@ -2,44 +2,48 @@
 	import { cn } from '$lib/utils';
 	import { generatePlaylistFile } from '../../services/generate_playlist_service';
 	import { Button } from '$lib/components/ui/button';
-	import { Download } from 'lucide-svelte';
+	import { FileText } from 'lucide-svelte';
+	import * as Tooltip from '$lib/components/ui/tooltip';
 
 	let className: string | undefined | null = undefined;
 	export let album;
 	export let aspectRatio: 'portrait' | 'square' = 'square';
-	export let width: number;
-	export let height: number;
 	export { className as class };
 	export let fetch: (input: URL | RequestInfo, init?: RequestInit | undefined) => Promise<Response>;
 </script>
 
-<div class={cn('space-y-3', className)} {...$$restProps}>
+<div class={cn('hover:bg-secondary mb-4x space-y-4 rounded-md p-2', className)} {...$$restProps}>
 	<div>
 		<div class="overflow-hidden rounded-md">
 			<img
 				class={cn(
-					'h-auto w-auto object-cover transition-all hover:scale-105',
+					'h-auto w-auto object-cover',
 					aspectRatio === 'portrait' ? 'aspect-[3/4]' : 'aspect-square'
 				)}
 				src={album.images[0].url}
 				alt={album.name}
-				{width}
-				{height}
 			/>
 		</div>
 	</div>
-	<Button
-		on:click={async () => {
-			await generatePlaylistFile(fetch, album.tracks.href, album.name);
-		}}
-	>
-		<Download class="mr-2 h-4 w-4" />
-		To Text
-	</Button>
 	<div class="space-y-1 text-sm">
 		<h3 class="font-medium leading-none">{album.name}</h3>
 		<p class="text-muted-foreground text-xs">
-			{album.type === 'playlist' ? album.description : album.artists[0]}
+			{album.type === 'playlist' ? '' : album.artists[0]}
 		</p>
 	</div>
+	<Tooltip.Root>
+		<Tooltip.Trigger>
+			<Button
+				on:click={async () => {
+					await generatePlaylistFile(fetch, album.tracks.href, album.name);
+				}}
+				class="relative"
+			>
+				<FileText class="h-4 w-4" />
+			</Button>
+		</Tooltip.Trigger>
+		<Tooltip.Content>
+			<p>Extract as text file</p>
+		</Tooltip.Content>
+	</Tooltip.Root>
 </div>
