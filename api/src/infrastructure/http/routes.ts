0a727e963d1e6ca76e5b917ef14f8967fa5baf0e
Thomasevano <thomas-evano@hotmail.fr>
2024-10-11 22:31:30 +0200

    feat(api): add SpotifyController and related routes

--- api/src/infrastructure/http/routes.ts
+++ api/src/infrastructure/http/routes.ts
@@ -8,9 +8,27 @@
 */
 
 import router from '@adonisjs/core/services/router'
+const SpotifyController = () => import('./controllers/spotify_controller.js')
 
-router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
-})
+router
+  .group(async () => {
+    router.get('/', async () => {
+      return {
+        hello: 'world',
+      }
+    })
+
+    router
+      .group(() => {
+        router
+          .group(() => {
+            router.get('login', [SpotifyController, 'login'])
+            router.get('callback', [SpotifyController, 'callback'])
+            router.get('refresh', [SpotifyController, 'refreshToken'])
+            router.get('logout', [SpotifyController, 'logout'])
+          })
+          .prefix('/spotify')
+      })
+      .prefix('/auth')
+  })
+  .prefix('/api')
