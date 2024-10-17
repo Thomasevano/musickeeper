2584541c1c85b8b3ec7a9797b11b221bdc6abe14
Thomasevano <thomas-evano@hotmail.fr>
2024-10-17 21:59:07 +0200

    fixup! feat: handle expired spotify_access_token

--- api/src/infrastructure/http/middleware/spotify_middleware.ts
+++ api/src/infrastructure/http/middleware/spotify_middleware.ts
@@ -3,19 +3,19 @@ import { HttpContext } from '@adonisjs/core/http'
 
 export default class SpotifyMiddleware {
   async handle(ctx: HttpContext, next: NextFn) {
-    const spotifyAccessTokenExpiresAt = ctx.request.cookie('spotify_access_token_expires-at')
-    const spotifyRefreshToken = ctx.request.cookie('spotify_refresh_token')
+    const spotifyAccessTokenExpiresAt = ctx.request.plainCookie('spotify_access_token_expires-at')
+    const spotifyRefreshToken = ctx.request.encryptedCookie('spotify_refresh_token')
 
     const currentDate = Math.floor(Date.now() / 1000)
     const difference = Math.floor((Number.parseInt(spotifyAccessTokenExpiresAt) - currentDate) / 60)
-    console.log({ difference, currentDate, spotifyAccessTokenExpiresAt })
-    if (difference > 1) {
-      return next()
-    } else {
+    console.log({ difference, currentDate, spotifyAccessTokenExpiresAt, spotifyRefreshToken })
+    if (difference < 1) {
+      console.log('access token invalid')
       await fetch(`${process.env.BASE_URL}/api/auth/spotify/refresh`, {
-        headers: { Cookie: `spotify_refresh_token=${spotifyRefreshToken}` },
+        headers: { cookie: `spotify_refresh_token=${spotifyRefreshToken}` },
       })
       return next()
     }
+    return next()
   }
 }
