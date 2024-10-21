9078cccd1ffd9aed0d0fffeb6c8b50d88eef26f5
Thomasevano <thomas-evano@hotmail.fr>
2024-10-21 19:08:54 +0200

    fixup! chore: static render lanfing page

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -2,11 +2,8 @@
 	import HeroSection from '$components/landing/HeroSection.svelte';
 	import Features from '$components/landing/Features.svelte';
 	import Navigation from '$components/Navigation.svelte';
-	import type { LayoutData } from './$types';
-	export let data: LayoutData;
-	$: ({ user, isLoggedIn } = data);
 </script>
 
-<Navigation {user} {isLoggedIn} />
-<HeroSection {isLoggedIn} />
+<Navigation />
+<HeroSection />
 <Features />
