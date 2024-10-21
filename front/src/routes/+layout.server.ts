e3549b816ec538b87899a7f36b8c718518c63b74
Thomasevano <thomas-evano@hotmail.fr>
2024-10-21 18:56:07 +0200

    chore: prerender landing page

--- front/src/routes/+layout.server.ts
+++ front/src/routes/+layout.server.ts
@@ -1,15 +1,1 @@
-import type { LayoutServerLoad } from './$types';
-
-export type OutputType = { user: object; isLoggedIn: boolean };
-export const load: LayoutServerLoad = async ({ locals }) => {
-	const user = locals.user;
-	if (user) {
-		// Return the output object
-		return { user, isLoggedIn: true };
-	}
-	// Return the output object
-	return {
-		user: undefined,
-		isLoggedIn: false
-	};
-};
+export const prerender = true;
