2cdb45e969cc10edc43c1bd308820e6c302c0f43
Thomasevano <thomas-evano@hotmail.fr>
2025-03-07 17:27:15 +0100

    ci: add test workflow

--- tests/unit/home.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test } from '@japa/runner'
-
-test('returns correct data', async ({ client }) => {
-  const response = await client.get('/').withInertia()
-
-  response.assertStatus(200)
-  response.assertInertiaComponent('home')
-})
