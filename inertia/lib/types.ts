89a583c22b9758bb70aefea25f6dc6cc174ad1d7
Thomasevano <thomas-evano@hotmail.fr>
2025-06-09 18:28:59 +0200

    refactor: feature icon display and add listen later

--- inertia/lib/types.ts
+++ inertia/lib/types.ts
@@ -1,6 +1,9 @@
-interface IFeature {
-  icon: string
-  title: string;
-  description: string;
-  isAvailable: Boolean;
+import { Component } from 'svelte'
+import type { IconProps } from '@lucide/svelte'
+
+export interface LandingFeature {
+  icon: Component<IconProps, {}, ''> | string
+  title: string
+  description: string
+  isAvailable: Boolean
 }
