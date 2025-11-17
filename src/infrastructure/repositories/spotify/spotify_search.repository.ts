78184545516809b12b701b83e33b1342dc3376e7
Thomasevano <thomas-evano@hotmail.fr>
2025-11-17 21:27:07 +0100

    feat: add album type

--- src/infrastructure/repositories/spotify/spotify_search.repository.ts
+++ src/infrastructure/repositories/spotify/spotify_search.repository.ts
@@ -1,8 +1,8 @@
 import { SearchRepository } from '../../../application/repositories/search.repository.js'
 
 export class SpotifySearchRepository implements SearchRepository {
-  async searchItem(query: string, token: string): Promise<SpotifyApi.SearchResponse> {
-    const searchUrl = `${process.env.SPOTIFY_BASE_URL}/search?q=${encodeURIComponent(query)}&type=track&limit=5`
+  async searchItem(query: string, token: string, type: string = 'track'): Promise<SpotifyApi.SearchResponse> {
+    const searchUrl = `${process.env.SPOTIFY_BASE_URL}/search?q=${encodeURIComponent(query)}&type=${type}&limit=5`
     const response = await fetch(searchUrl, {
       headers: {
         Authorization: `Bearer ${token}`,
