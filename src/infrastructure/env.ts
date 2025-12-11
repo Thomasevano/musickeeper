3562075255fd0d5781af5d3bff334bec82125c79
Thomasevano <thomas-evano@hotmail.fr>
2025-12-11 18:18:18 +0100

    build: remove env var HOST & clean .env.example

--- src/infrastructure/env.ts
+++ src/infrastructure/env.ts
@@ -15,7 +15,6 @@ export default await Env.create(new URL('../..', import.meta.url), {
   NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
   PORT: Env.schema.number(),
   APP_KEY: Env.schema.string(),
-  HOST: Env.schema.string({ format: 'host' }),
   LOG_LEVEL: Env.schema.enum(['fatal', 'error', 'warn', 'info', 'debug', 'trace']),
 
   /*
