e6092c09c7e47b00a010ca9406d81aad6cfc226a
Thomasevano <thomas-evano@hotmail.fr>
2025-12-15 21:43:58 +0100

    content: actualize landing page to match new functionnality

--- inertia/components/landing/Features.svelte
+++ inertia/components/landing/Features.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { ListEnd, ArrowRight, FolderKanban, Download } from '@lucide/svelte'
+  import { ListEnd, FolderKanban, Download } from '@lucide/svelte'
   import type { LandingFeature } from '~/lib/types'
   import FeatureCard from './FeatureCard.svelte'
 
@@ -11,21 +11,9 @@
       isAvailable: true,
     },
     {
-      icon: ArrowRight,
-      title: 'Transfer',
-      description: 'Transfer your music library from one streaming service to another',
-      isAvailable: false,
-    },
-    {
       icon: FolderKanban,
       title: 'Manage',
       description: 'Centralize all your music library from streaming services all in one place',
-      isAvailable: false,
-    },
-    {
-      icon: 'connect',
-      title: 'Connect',
-      description: 'Connect to all streaming services',
       isAvailable: false,
     },
     {
@@ -41,19 +29,18 @@
   id="features"
   class="container space-y-6 py-8 md:py-12 lg:pt-0 lg:py-24 dark:bg-transparent"
 >
-  <div class="mx-auto flex max-w-[58rem] flex-col items-center space-y-4 text-center">
+  <div class="mx-auto flex max-w-232 flex-col items-center space-y-4 text-center">
     <h2 class="font-heading text-3xl leading-[1.1] sm:text-3xl md:text-6xl">Features</h2>
     <p
       class="balance-text text-muted-foreground max-w-[85%] leading-normal sm:text-lg sm:leading-7"
     >
       The philosophy of this project started from a desire to have more control and freedom on my
       music and be less dependant of music streaming service. So if tomorrow the one you use doesn't
-      exist anymore, you are able keep the content of playlists, saved albums and artists. It also
-      aim to help you switch from music streaming service and be able to legally buy your favorite
-      music in one click.
+      exist anymore, you are have control over your music library. It also aim to help you own your
+      music back and supports artists by legally buy your favorite music in one click.
     </p>
   </div>
-  <div class="mx-auto grid justify-center gap-4 sm:grid-cols-2 md:max-w-[64rem] md:grid-cols-3">
+  <div class="mx-auto grid justify-center gap-4 sm:grid-cols-2 md:max-w-5xl md:grid-cols-3">
     {#each features as feature (feature.icon)}
       <FeatureCard {feature} />
     {/each}
--- inertia/components/landing/HeroSection.svelte
+++ inertia/components/landing/HeroSection.svelte
@@ -4,13 +4,13 @@
 </script>
 
 <section class="space-y-6 pb-6 pt-16 lg:py-28">
-  <div class="container flex max-w-[64rem] flex-col items-center gap-5 text-center">
+  <div class="container flex max-w-5xl flex-col items-center gap-5 text-center">
     <a
       href="https://github.com/Thomasevano/musickeeper"
       class={cn(buttonVariants({ variant: 'outline', size: 'sm' }))}
       target="_blank"
     >
-      source code and releases on Github <i class="si si-github si--color ml-2 mr-2 text-2xl"></i>
+      source code is available on Github <i class="si si-github si--color ml-2 mr-2 text-2xl"></i>
       <span aria-hidden="true">&rarr;</span>
     </a>
 
@@ -20,11 +20,9 @@
       MusicKeeper
     </h1>
 
-    <p
-      class="text-muted-foreground max-w-[42rem] text-balance leading-normal sm:text-xl sm:leading-8"
-    >
-      A music manager app that help you manage, extract and transfer your music between different
-      streaming platforms
+    <p class="text-muted-foreground max-w-2xl text-balance leading-normal sm:text-xl sm:leading-8">
+      A music manager app that help you manage and keep track on music you want or have been
+      recommanded to listen to.
     </p>
     <div class="flex justify-center space-x-2 md:space-x-4">
       <Button size="lg" href="{import.meta.env.VITE_BASE_URL}/auth/spotify/login">
