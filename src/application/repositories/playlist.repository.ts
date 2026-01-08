b0c2129ab3185ed6091bb93959e7e50fe8de7ddd
Thomasevano <thomas-evano@hotmail.fr>
2026-01-08 19:36:43 +0100

    refactor: remove unnecessary code related to the spotify api

--- src/application/repositories/playlist.repository.ts
+++
@@ -1,8 +0,0 @@
-import { PaginatedPlaylistsInfos } from '../../domain/playlist.js'
-import { Track } from '../../domain/track.js'
-
-export abstract class PlaylistRepository {
-  abstract getCurrentUserPlaylistsInfos(token: string): Promise<PaginatedPlaylistsInfos>
-  abstract getUserPlaylistsInfos(token: string, url: string): Promise<PaginatedPlaylistsInfos>
-  abstract getSongsFromPlaylist(playlistUrl: string, token: string): Promise<Track[]>
-}
--- src/infrastructure/http/routes.ts
+++ src/infrastructure/http/routes.ts
@@ -8,37 +8,13 @@
 */
 
 import router from '@adonisjs/core/services/router'
-import { middleware } from './kernel.js'
-const SpotifyController = () => import('./controllers/spotify_controller.js')
 const HomeController = () => import('./controllers/home_controller.js')
-const PlaylistController = () => import('./controllers/playlists_controller.js')
 const ListenLaterListController = () => import('./controllers/listen_later_list_controller.js')
-const ErrorsController = () => import('./controllers/errors_controllers.js')
 
 router.get('/', [HomeController, 'index'])
 
 router
   .group(() => {
-    router.get('login', [SpotifyController, 'login'])
-    router.get('callback', [SpotifyController, 'callback']).use(middleware.shareUser())
-    router.get('refresh', [SpotifyController, 'refreshToken'])
-    router.get('logout', [SpotifyController, 'logout'])
-  })
-  .prefix('/spotify')
-  .prefix('/auth')
-
-router
-  .group(() => {
-    router
-      .group(() => {
-        router.get('/', [PlaylistController, 'index'])
-      })
-      .prefix('/playlists')
     router.get('/listen-later', [ListenLaterListController, 'index'])
   })
   .prefix('/library')
-  .use(middleware.spotifyAuthCheck())
-  .use(middleware.spotifyRefreshToken())
-  .use(middleware.shareUser())
-
-router.get('/errors/unauthorized_access', [ErrorsController, 'unauthorizedAccess'])
