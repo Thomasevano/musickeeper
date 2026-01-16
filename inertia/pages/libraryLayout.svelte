6922733511f6e322d4efbc0bd075819ed36ea81e
Thomasevano <thomas-evano@hotmail.fr>
2026-01-16 18:42:54 +0100

    feat(ui): remove sidebar and use navbar instead

--- inertia/pages/libraryLayout.svelte
+++ inertia/pages/libraryLayout.svelte
@@ -1,16 +1,10 @@
 <script>
-  import Navigation from '~/components/Navigation.svelte'
   import Layout from '~/layouts/layout.svelte'
-  import * as Sidebar from '$lib/components/ui/sidebar'
-  import AppSidebar from '~/components/AppSidebar.svelte'
   let { children, data, title, description } = $props()
 </script>
 
 <Layout {title} {description}>
-  <Sidebar.Provider>
-    <AppSidebar />
-    {#if data}
-      {@render children()}
-    {/if}
-  </Sidebar.Provider>
+  {#if data}
+    {@render children()}
+  {/if}
 </Layout>
