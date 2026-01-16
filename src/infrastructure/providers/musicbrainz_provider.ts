5ae5e2ce1242d434056d1195da2d80d5cd2e6760
Thomasevano <thomas-evano@hotmail.fr>
2026-01-16 19:44:44 +0100

    fix(api): access package.json varaibles in production for musicbrainz api

--- src/infrastructure/providers/musicbrainz_provider.ts
+++ src/infrastructure/providers/musicbrainz_provider.ts
@@ -2,6 +2,7 @@ import type { ApplicationService } from '@adonisjs/core/types'
 import { CoverArtArchiveApi, MusicBrainzApi } from 'musicbrainz-api'
 import { SearchRepository } from '../../application/repositories/search.repository.js'
 import { MusicBrainzRepository } from '../repositories/musicbrainz_search.repository.js'
+import { readFileSync } from 'node:fs'
 
 export default class MusicBrainzProvider {
   constructor(protected app: ApplicationService) {}
@@ -11,11 +12,14 @@ export default class MusicBrainzProvider {
     })
   }
 }
+const packageJson = JSON.parse(
+  readFileSync(new URL('../../../package.json', import.meta.url), 'utf8')
+)
 
 export const musicbrainzApi = new MusicBrainzApi({
-  appName: process.env.APP_NAME,
-  appVersion: process.env.APP_VERSION,
-  appContactInfo: process.env.APP_CONTACT_EMAIL,
+  appName: packageJson.name,
+  appVersion: packageJson.version,
+  appContactInfo: process.env.MB_APP_CONTACT_EMAIL,
 })
 
 export const coverArtArchiveApiClient = new CoverArtArchiveApi()
