0bd73c658f0885113f414799a92129e9f0639322
Thomasevano <thomas-evano@hotmail.fr>
2026-04-05 16:49:28 +0200

    fix(ui): toggleTheme animation

--- inertia/components/ToggleTheme.svelte
+++ inertia/components/ToggleTheme.svelte
@@ -9,10 +9,10 @@
 <DropdownMenu.Root>
   <DropdownMenu.Trigger class={buttonVariants({ variant: 'ghost', size: 'icon' })}>
     <Sun
-      class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+      class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-[transform,opacity] dark:-rotate-90 dark:scale-0"
     />
     <Moon
-      class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+      class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-[transform,opacity] dark:rotate-0 dark:scale-100"
     />
     <span class="sr-only">Toggle theme</span>
   </DropdownMenu.Trigger>
